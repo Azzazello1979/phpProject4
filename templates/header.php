22f9575515e5cecec53fdb2b9ceae7d2d0ea9315
Balint H <balint.haui@gmail.com>
2020-03-11 11:27:39 +0100

    header.php added

---
+++ templates/header.php
@@ -0,0 +1,27 @@
+<?php
+
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <style type="text/css">
+        .brand{
+            background: #cbb09c !important;
+        }
+        .brand-text{
+            color: #cbb09c !important;
+        }
+    </style>
+    <title>PHP Project 4</title>
+</head>
+<body class="grey lighten-4">
+    <nav class="white z-depth-0">
+        <div class="container">
+            <a href="#" class="brand-logo brand-text">Project 4</a>
+            <ul id="nav-mobile" class="right hide-on-small-and-down">
+                <li><a href="#" class="btn brand z-depth-0">Add project</a></li>
+            </ul>
+        </div>
+    </nav>
