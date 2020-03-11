f18b164b16b199b2139613237ff08dc5a540bcd1
Balint H <balint.haui@gmail.com>
2020-03-11 15:15:06 +0100

    form values persist, form value initialized

--- add.php
+++ add.php
@@ -1,34 +1,40 @@
 <?php
+    $title = $email = $ingredients = '';
+    $errors = [
+        'email'=>'',
+        'title'=>'',
+        'details'=>''
+    ];
 
     if(isset($_POST['submit'])){
 
         // check email
         if(empty($_POST['email'])){
-            echo 'email is required <br />' ;
+            $errors['email'] = 'email cannot be empty';
         } else {
             $email = $_POST['email'];
             if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-                echo 'email must be valid';
+                $errors['email'] = 'email must be valid';
             }
         }
 
         // check title
         if(empty($_POST['title'])){
-            echo 'title is required <br />' ;
+            $errors['title'] = 'title cannot be empty';
         } else {
             $title = $_POST['title'];
             if(!preg_match( '/^[a-zA-Z\s]+$/', $title )){
-                echo 'title must be letters and spaces only';
+                $errors['title'] = 'title must be letters and spaces only';
             }
         }
 
         // check details
         if(empty($_POST['details'])){
-            echo 'details is required <br />' ;
+            $errors['details'] = 'details cannot be empty';
         } else {
             $details = $_POST['details'];
             if(!preg_match( '/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $details )){
-                echo 'details must be comma separated list';
+                $errors['details'] = 'details must be comma separated list';
             }
         }
     }
@@ -44,11 +50,14 @@
         <h4 class="center">Add project</h4>
         <form method="POST" action="add.php" class="white">
             <label>Your email:</label>
-            <input type="text" name="email">
+            <input type="text" name="email" value="<?php echo $_POST['email']; ?>">
+                <div class="red-text"><?php echo $errors['email']; ?></div>
             <label>Project title:</label>
-            <input type="text" name="title">
+            <input type="text" name="title" value="<?php echo $_POST['title']; ?>">
+                <div class="red-text"><?php echo $errors['title']; ?></div>
             <label>Project details (comma separated list):</label>
-            <input type="text" name="details">
+            <input type="text" name="details" value="<?php echo $_POST['details']; ?>">
+                <div class="red-text"><?php echo $errors['details']; ?></div>
             <div class="center">
                 <input type="submit" name="submit" value="submit" class="btn brand z-depth-0" />
             </div>
